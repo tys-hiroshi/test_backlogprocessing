ec6e7999e02b3bae112e51625fa2c458938037a0
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-09-16 13:30:12 +0900

    install chatworkpy

--- main.py
+++ main.py
@@ -9,7 +9,20 @@ import sys
 from backlogapiprocessmodule import *
 import os
 
-ConfigFilePath = os.path.join(os.getcwd(), "config.yml")
-LoggingConfigFilePath = os.path.join(os.getcwd(), "logging_debug.conf")
-backlogapiprocess.run(ConfigFilePath, LoggingConfigFilePath)
+from chatworkpy.chatwork.rooms import Rooms
+from chatworkpy.config import Config
 
+ConfigFilePath = os.path.join(os.getcwd(), "config.yml")
+try:
+    LoggingConfigFilePath = os.path.join(os.getcwd(), "logging_debug.conf")
+    raise ValueError("Error aaaaa")
+    backlogapiprocess.run(ConfigFilePath, LoggingConfigFilePath)
+except Exception as e:
+    chatwork_config = Config(ConfigFilePath).content["ALERT"]
+    api_token = chatwork_config["CHATWORK_API_TOKEN"]
+    room_id = chatwork_config["CHATWORK_ROOM_ID"]
+    to_id_list = chatwork_config["CHATWORK_TO_ID_LIST"].split(',')
+    chatwork_rooms = Rooms(api_token, room_id)
+    import traceback
+    error_message = traceback.format_exc()
+    chatwork_rooms.send_message(error_message, to_id_list)
